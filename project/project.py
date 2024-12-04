61e613b870f28facb538ba3b6747b47ce13a0632
Osman Batuhan İnalöz <93053512+ydsaglme@users.noreply.github.com>
2024-12-04 19:42:45 +0300

    Update project.py

--- project/project.py
+++ project/project.py
@@ -128,8 +128,8 @@ class stock_analyzer:
             axs[1, 1].legend(fontsize = 6, loc = "lower right")
 
             axs[2, 1].hist(historical_data["Daily Return"], bins = 25, color = "blue")
-            axs[2, 1].set_xlabel("Daily Return (%)", size=8)
-            axs[2, 1].set_ylabel("Counts", size=8)
+            axs[2, 1].set_xlabel("Daily Return (%)", size = 8)
+            axs[2, 1].set_ylabel("Counts", size = 8)
 
             plt.tight_layout()
             plt.show()
